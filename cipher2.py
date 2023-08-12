029b3ff20fd2d3527c096a44a98065c9426ed175
metrictulip0309 <134560227+metrictulip0309@users.noreply.github.com>
2023-08-12 19:59:06 -0400

    Add files via upload

---
+++ cipher2.py
@@ -0,0 +1,22 @@
+# add your code here
+def encypt_func(txt, s):  
+    result = ""  
+  
+  
+# transverse the plain txt  
+    for i in range(len(txt)):  
+        char = txt[i]  
+        # encypt_func uppercase characters in plain txt  
+  
+        if (char.isupper()):  
+            result += chr((ord(char) + s - 64) % 26 + 65)  
+        # encypt_func lowercase characters in plain txt  
+        else:  
+            result += chr((ord(char) + s - 96) % 26 + 97)  
+    return result  
+# check the above function  
+txt = input("Please enter a sentence: ")  
+s = 5
+
+print("The encrypted sentence is: " + encypt_func(txt, s))
+
