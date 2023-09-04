8dc9b62e6ec6f0788cc771025fa331426ba241d3
metrictulip0309 <eclairez995@gmail.com>
2023-09-04 16:13:48 -0400

    cipher updated

--- cipher.py
+++ cipher.py
@@ -3,31 +3,29 @@
 
 # In[ ]:
 
-
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
             'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 txt = input("Please enter a sentence: ").lower()
 shift = 5
 
-def is_abc(value, list):
-    #Check if character is contained in list
-    for x in list:
-        if x == value:
+def is_alphabet(value, list):
+    #loop through list to see if character is contained
+    for i in list:
+        if i == value:
             return True
         False 
 
-def enc_text(text_string, shift):
-    #encrypt each letter by specified shift amount
-    encr_list=[]
+def encrypt_text(text_string, shift):
+    #encrypt alphabetical value by shift amount
+    encrypted_list=[]
 
     for plain_text in text_string:
-        if is_abc(plain_text, alphabet):
+        if is_alphabet(plain_text, alphabet):
             index=int(alphabet.index(plain_text))
             plain_text=alphabet[index + shift]
-        encr_list.append(plain_text)
-    encrypted_string = "".join(encr_list)
+        encrypted_list.append(plain_text)
+    encrypted_string = "".join(encrypted_list)
     print(f"The encrypted sentence is: {encrypted_string}")
 
-enc_text(text_string=txt,shift=shift)
-
+encrypt_text(text_string=txt,shift=shift)
